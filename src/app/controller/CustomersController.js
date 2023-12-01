19331d1f9a51d8408362392c56cfb1c0891b519c
Simas7 <simasrenan@hotmail.com>
2023-12-01 13:38:39 -0300

    .

---
+++ src/app/controller/CustomersController.js
@@ -0,0 +1,35 @@
+class CustomersControllers {
+    Constructor() {
+        let customers = [
+            { id: 1, name: "Renan Simas", idade: 20, info: ""},
+            {id: 2, name: "Alexandre Fernandes", idade: "22", info: ""},
+            {id: 3, name: "Camila Soares", idade: "22", info:""},
+            {id: 4, name: "Guilherme Damasceno", idade:"23", info: ""}
+        ];
+    }
+
+    //LISTAGEM DOS REGISTROS
+    index(req, res){
+
+    }
+
+    //RECUPERA UM REGISTRO
+    show(req, res){
+
+    }
+
+    //CRIA UM REGISTRO
+    create(req, res){
+
+    }
+
+    //ATUALIZA UM REGISTRO
+    update(req, res){
+
+    }
+
+    //DELETA UM REGISTRO
+    destroy(req, res){
+        
+    }
+}
